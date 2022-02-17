5b743b0fdbf8585d31217da38345cac1bfbea4ec
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-02-17 20:44:20 +0100

    description updated

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -3,7 +3,10 @@ const UsersIndex = () => {
     return (
         <div>
             <h1>directory &quot;users&quot;, index page</h1>
-            <p>unavailable because &quot;users.tsx&quot; from the main directory is displayed instead</p>
+            <p>
+                displayed by vercel but not in the version that runs locally
+                (&quot;users.tsx&quot; from the main directory is displayed instead)
+            </p>
         </div>
     )
 }
